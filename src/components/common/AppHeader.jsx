b46a853abd0c4cb3d376a6af81bf063eb9372811
smasherjupiter <tonylobbin7712@gmail.com>
2022-09-16 17:16:48 -0700

    w3 connect

--- src/components/common/AppHeader.jsx
+++ src/components/common/AppHeader.jsx
@@ -1,4 +1,10 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+
+
+import Web3Modal from "web3modal";
+import { ethers } from "ethers";
+import WalletConnectProvider from "@walletconnect/web3-provider";
+import Web3 from "web3"
 
 import { Anchor } from 'antd';
 import { Drawer, Button } from 'antd';
@@ -11,7 +17,140 @@ const { Option } = Select;
 
 
 
+
+       
+let web3Modal;
+let provider;
+let selectedAccount;
+
+// import Web3 from 'web3'
+function init() {
+    const providerOptions = {
+        walletconnect: {
+        package: WalletConnectProvider,
+        options: {
+            networ:'rinkeby',
+            rpc: {
+                4:"https://rinkeby.infura.io/v3/"
+            },
+            chainId:4
+        }
+        },           
+    };
+
+    web3Modal = new Web3Modal({
+        network: "mainnet", // optional
+        cacheProvider: true, // optional
+        providerOptions // required
+    });
+
+    window.w3m = web3Modal;
+}
+
+async function fetchAccountData() {
+    const web3Provider = new ethers.providers.Web3Provider(provider);
+    const signer = web3Provider.getSigner();
+    selectedAccount = await signer.getAddress();
+    console.log(selectedAccount);    
+    return selectedAccount;
+}
+
+async function refreshAccountData() {
+    await fetchAccountData(provider);
+    window.location.reload();
+}
+
+async function onConnect(){
+    console.log("Opening a dialog", web3Modal);
+    try {
+        provider = await web3Modal.connect({ cacheProvider: true });
+    } catch (e) {
+        console.log("Could not get a wallet connection", e);
+        return;
+    }
+
+    provider.on("accountsChanged", (accounts) => {
+        console.log('chainchan',accounts)
+        fetchAccountData();
+        window.location.reload();
+    });
+
+    provider.on("chainChanged", (chainId) => {
+        fetchAccountData();
+        window.location.reload();
+    });
+
+    provider.on("networkChanged", (networkId) => {
+        fetchAccountData();
+        window.location.reload();
+    });
+    // window.location.reload()
+
+    await refreshAccountData();
+}
+
+async function disconnet() {
+    console.log("Opening a dialog", web3Modal);
+    try {
+        // provider = await web3Modal.connect();
+        await web3Modal.clearCachedProvider();
+        // await window.ethereum.disable()
+        window.location.reload()
+    } catch (e) {
+        console.log("Could not get a wallet connection", e);
+        return;
+    }   
+}
+
 function AppHeader() {
+
+    const [acc,setacc] = useState()
+
+    useEffect(() => {
+        init();
+        getAccount();
+        if (web3Modal.cachedProvider) {
+            setacc(true)
+        }
+    }, []); 
+
+    async function getAccount() {
+        // const web3_2 = new Web3(window.ethereum, null, { transactionConfirmationBlocks: 1 })
+        if (window.ethereum) {
+            // request change chain
+            try {
+                await window.ethereum.request({
+                    method: 'wallet_switchEthereumChain',
+                    params: [{ chainId: '0x04' }],
+                });
+            } catch (switchError) {
+                // This error code indicates that the chain has not been added to MetaMask.
+                if (switchError.code === 4902) {
+                    try {
+                        const data = [{
+                            chainId: '0x01',
+                            chainName: 'Ethereum Chain',
+                            nativeCurrency: {
+                            name: 'ETH',
+                            symbol: 'ETH',
+                            decimals: 18,
+                            },
+                            rpcUrls: ['https://mainnet.infura.io/v3/'],
+                            blockExplorerUrls: ['https://etherscan.io'],
+                        }]
+
+                        await window.ethereum.request({
+                            method: 'wallet_addEthereumChain',
+                            params: data,
+                        });
+                    } catch (addError) {
+                        
+                    }
+                }
+            }
+        } 
+    }
+
     const { Link } = Anchor;
     // const [size, setSize] = useState<SizeType>('large');
     const [visible, setVisible] = useState(false);
@@ -53,7 +192,7 @@ function AppHeader() {
                             <Link href="#resourses" title="Resourses" />
                             <Link href="#works" title="Create" />
                         </Anchor>
-                        <Button type="primary" className='connect-btn mobile-state-btn' shape="round" size="large">
+                        <Button type="primary" onClick={()=> onConnect()} className='connect-btn mobile-state-btn' shape="round" size="large">
                             Connect Wallet
                         </Button>
                     </Drawer>
@@ -70,7 +209,7 @@ function AppHeader() {
                     </Anchor>
                 </div>
 
-                <Button type="primary" className='connect-btn mobileHidden' shape="round" size="large">
+                <Button type="primary" onClick={()=> onConnect()} className='connect-btn mobileHidden' shape="round" size="large">
                     Connect Wallet
                 </Button>
             </div>
