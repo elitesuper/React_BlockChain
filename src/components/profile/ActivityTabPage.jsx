e22594e2d9edf57dfca8c2746c25e81e984c44bf
smasherjupiter <tonylobbin7712@gmail.com>
2022-09-20 03:06:33 -0700

    menu item add

--- src/components/profile/ActivityTabPage.jsx
+++ src/components/profile/ActivityTabPage.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import {List, Row, Col, Collapse, Button, Divider} from 'antd';
+import React, {useState} from 'react';
+import {List, Row, Col, Menu, Button, Checkbox} from 'antd';
 import {TagOutlined} from '@ant-design/icons';
 
 import ch1 from '../../assets/collections/ch1.png'
@@ -7,7 +7,15 @@ import ch2 from '../../assets/collections/ch2.png'
 import ch3 from '../../assets/collections/ch3.png' 
 import ch4 from '../../assets/collections/ch4.png' 
 
-const {Panel} = Collapse;
+
+function getItem(label, key, children, type) {
+    return {
+      key,
+      children,
+      label,
+      type,
+    };
+}
 
 const data = [
     {
@@ -122,23 +130,44 @@ const data = [
     },
 ]
 function ActivityTabPage(){
+
+    const [filters, Setfilter] = useState(null);
+    
+    const customcheck =(val) =>{
+        return <div className='flex justify-between'>
+            <h5>{val}</h5>
+            <Checkbox></Checkbox>
+        </div>
+    }
+      const items = [
+        getItem('EventType', 'sub1', [
+          getItem(customcheck("Sales"), '1'),
+          getItem(customcheck('Listings'), '2'),
+          getItem(customcheck('Offers'), '3'),
+          getItem(customcheck('Collection offers'), '4'),
+          getItem(customcheck('Transfers'), '5'),
+        ]),
+        getItem('Collections', 'sub2', [
+          getItem(customcheck('Option 6'), '6'),
+          getItem(customcheck('Option 7'), '7'),
+        ]),
+        getItem('Chains', 'sub4', [
+          getItem(customcheck('Option 8'), '8'),
+          getItem(customcheck('Option 9'), '9'),
+        ]),
+      ];
+
     return(
         <div>
 
             <Row>
                 <Col md={{span:24}} lg={{span:6}}>
-                <Collapse defaultActiveKey={['1']} ghost>
-                    <Panel header="Event Type" key="1">
-                    <p>sfdadcdcasd</p>
-                    </Panel>
-                    <Divider/>
-                    <Panel header="Collections" key="2">
-                    <p>dfasdfasdf</p>
-                    </Panel>
-                    <Panel header="Chains" key="3">
-                    <p>fdafasd</p>
-                    </Panel>
-                </Collapse>
+                <Menu
+                    className='wd-90 m-t-28 mx-auto'
+                    mode="inline"
+                    defaultOpenKeys={['sub1']}
+                    items={items}
+                />
                 </Col>
                 <Col md={{span:24}} lg={{span:18}}>
                     <div className='pt-2 pd-12 w-full h-8'>
@@ -150,17 +179,17 @@ function ActivityTabPage(){
                     className='pd-b-30 nft-list'
                     header={<Row>
                             <Col className='text-center' span={9}>Item</Col>
-                            <Col className='text-center' span={4}>Price</Col>
+                            <Col className='text-center' span={3}>Price</Col>
                             <Col className='text-center' span-={2}>Quantity</Col>
                             <Col className='text-center' span={3}>From</Col>
                             <Col className='text-center' span={3}>To</Col>
-                            <Col className='text-center' span={3}>Time</Col>
+                            <Col className='text-center' span={4}>Time</Col>
                         </Row>}
                     itemLayout="horizontal"
                     dataSource={data}
                     renderItem={item => (
                     <Row className='activity-list'>
-                        <Col xs={{span:24}} sm={{span:24}} md={{span:12}} xl={{span:9}} className="flex">
+                        <Col xs={{span:24}} md={{span:9}} xl={{span:9}} className="flex">
                             <div className='flex justify-between'>
                                 <div className='flex'>
                                     <Button className='my-auto' size='large' type="text" icon={<TagOutlined  />}>
@@ -176,7 +205,7 @@ function ActivityTabPage(){
                                 </div>
                             </div>
                         </Col>
-                        <Col xs={{span:8}} md={{span:12}} xl={{span:4}}>
+                        <Col xs={{span:8}} md={{span:3}} xl={{span:3}}>
                             <div className='h-full flex'>
                                 <div className='m-auto'>
                                     <h5 className='text-sm col-spec-2'>{item.price}</h5>
@@ -184,28 +213,28 @@ function ActivityTabPage(){
                                 </div>
                             </div>
                         </Col>
-                        <Col xs={{span:8}} md={{span:6}} xl={{span:2}}>
+                        <Col xs={{span:8}} md={{span:2}} xl={{span:2}}>
                             <div className='h-full flex'>
                                 <div className='m-auto'>
                                     <h5>{item.quantity}</h5>                               
                                 </div>
                             </div>
                         </Col>
-                        <Col xs={{span:8}} md={{span:6}} xl={{span:3}}>
+                        <Col xs={{span:8}} md={{span:3}} xl={{span:3}}>
                             <div className='h-full flex'>
                                 <div className='m-auto'>
                                     <h5 className='col-spec-3'>{item.from}</h5>                               
                                 </div>
                             </div>
                         </Col>
-                        <Col xs={{span:12}} md={{span:6}} xl={{span:3}}>
+                        <Col xs={{span:12}} md={{span:3}} xl={{span:3}}>
                             <div className='h-full flex'>
                                 <div className='m-auto'>
                                     <h5>{item.to}</h5>                               
                                 </div>
                             </div>
                         </Col>
-                        <Col xs={{span:12}} md={{span:6}} xl={{span:3}}>
+                        <Col xs={{span:12}} md={{span:4}} xl={{span:4}}>
                             <div className='h-full flex'>
                                 <div className='m-auto'>
                                     <h5>{item.time}</h5>                               
